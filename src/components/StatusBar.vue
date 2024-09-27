f8f8b5e14eae62f74bf13cdb3e9ee58991c8a2b3
Juche <azhucheng1@qq.com>
2024-09-27 18:34:08 +0800

    update StatusBar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,44 +1,52 @@
 <template>
   <div data-tauri-drag-region class="titlebar">
-    <div
-      class="titlebar-button"
-      id="titlebar-minimize"
-      @click="appWindow.minimize"
-    >
-      <img src="https://api.iconify.design/line-md:minus.svg" alt="minimize" />
+    <div class="status-icon" @click="">
+      <img src="https://api.iconify.design/line-md:round-360.svg" />
     </div>
-    <div class="titlebar-button" id="titlebar-maximize" @click="toggleMaximize">
+    <div class="status-icon" @click="">
+      <img src="https://api.iconify.design/mingcute:random-line.svg" />
+    </div>
+    <div class="status-icon" @click="">
+      <img src="https://api.iconify.design/line-md:play.svg" />
+    </div>
+
+    <div class="status-icon" @click="">
+      <img src="https://api.iconify.design/line-md:volume-high.svg" />
+    </div>
+
+    <div class="status-icon" @click="appWindow.close">
+      <img src="https://api.iconify.design/line-md:close.svg" />
+    </div>
+    <div class="status-icon" @click="appWindow.minimize">
+      <img src="https://api.iconify.design/line-md:minus.svg" />
+    </div>
+    <div class="status-icon" @click="toggleMaximize">
       <img
         v-if="!isMaxSize"
         src="https://api.iconify.design/line-md:monitor-arrow-up.svg"
-        alt="maximize"
       />
       <img
         v-else
         src="https://api.iconify.design/line-md:monitor-arrow-down.svg"
-        alt="maximize"
       />
     </div>
+
+    <div class="status-icon status-icon-config" @click="">
+      <img src="https://api.iconify.design/line-md:cog-loop.svg" />
+    </div>
+
     <!-- <div
-      class="titlebar-button"
-      id="titlebar-fullscreen"
+      class="status-icon"
       @click="appWindow.setFullscreen(true)"
     >
-      <img
-        src="https://api.iconify.design/line-md:monitor-screenshot.svg"
-        alt="maximize"
-      />
+      <img src="https://api.iconify.design/line-md:monitor-screenshot.svg" />
     </div>
     <div
-      class="titlebar-button"
-      id="titlebar-fullscreen"
+      class="status-icon"
       @click="appWindow.setFullscreen(false)"
     >
-      <img src="https://api.iconify.design/line-md:laptop.svg" alt="maximize" />
+      <img src="https://api.iconify.design/line-md:laptop.svg" />
     </div> -->
-    <div class="titlebar-button" id="titlebar-close" @click="appWindow.close">
-      <img src="https://api.iconify.design/line-md:close.svg" alt="close" />
-    </div>
   </div>
 </template>
 
@@ -58,6 +66,7 @@
 <style scoped>
   .titlebar {
     display: flex;
+    /* flex-direction: row-reverse; */
     justify-content: flex-end;
     align-items: center;
     position: fixed;
@@ -68,14 +77,23 @@
     background: #eee;
     user-select: none;
   }
-  .titlebar-button {
+  .status-icon {
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    width: 30px;
-    height: 30px;
+    width: 32px;
+    height: 32px;
   }
-  .titlebar-button:hover {
+  .status-icon:hover {
     background: #5bbec3;
+  }
+
+  .status-icon img {
+    width: 20px;
+    height: 20px;
+  }
+  .status-icon-config img {
+    width: 32px;
+    height: 32px;
   }
 </style>
