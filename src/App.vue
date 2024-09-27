4001cd55a2ba4de7cb9e71b380121dbd8596c762
Juche <azhucheng1@qq.com>
2024-09-27 12:13:59 +0800

    添加闪卡基础交互配置 & 全局样式处理

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,17 @@
 
 <style>
   #app {
-    /* font-family: Avenir, Helvetica, Arial, sans-serif;
+    /* margin-top: 60px; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px; */
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
   }
 </style>
--- src/components/Flashcard.vue
+++ src/components/Flashcard.vue
@@ -1,15 +1,75 @@
 <template>
+  <!-- :centeredSlides="true" -->
+  <!-- :spaceBetween="30" -->
   <swiper
-    :effect="'cards'"
+    :autoplay="{
+      delay: 250000,
+      disableOnInteraction: false,
+    }"
     :grabCursor="true"
+    :effect="'cube'"
+    :cubeEffect="{
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    }"
+    :keyboard="{
+      enabled: true,
+    }"
+    :loop="true"
     :modules="modules"
+    :navigation="true"
+    :pagination="{
+      clickable: true,
+      type: 'fraction', // 数字分页
+    }"
+    :slidesPerView="1"
+    :zoom="true"
     class="mySwiper"
   >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    <swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+      </div> </swiper-slide
+    ><swiper-slide>
+      <div class="swiper-zoom-container">
+        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+      </div>
+    </swiper-slide>
+    <!-- <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide>
+    <swiper-slide>Slide 9</swiper-slide> -->
   </swiper>
 </template>
 <script>
@@ -18,13 +78,26 @@
 
   // Import Swiper styles
   import 'swiper/css'
-
   import 'swiper/css/effect-cards'
+  import 'swiper/css/effect-creative'
+  import 'swiper/css/effect-cube'
+  import 'swiper/css/navigation'
+  import 'swiper/css/pagination'
+  import 'swiper/css/zoom'
 
   import '../style.css'
 
   // import required modules
-  import { EffectCards } from 'swiper/modules'
+  import {
+    Autoplay,
+    EffectCards,
+    EffectCreative,
+    EffectCube,
+    Keyboard,
+    Navigation,
+    Pagination,
+    Zoom,
+  } from 'swiper/modules'
 
   export default {
     components: {
@@ -33,7 +106,16 @@
     },
     setup() {
       return {
-        modules: [EffectCards],
+        modules: [
+          Autoplay,
+          EffectCards,
+          EffectCreative,
+          EffectCube,
+          Keyboard,
+          Pagination,
+          Navigation,
+          Zoom,
+        ],
       }
     },
   }
