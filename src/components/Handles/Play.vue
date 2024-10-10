9ac0e1377ba35b1f718ec7d46fe552d1f0baf12e
Juche <azhucheng1@qq.com>
2024-10-10 18:32:40 +0800

    update

--- src/components/Handles/Play.vue
+++ src/components/Handles/Play.vue
@@ -7,16 +7,37 @@
       <!-- <img src="https://api.iconify.design/mingcute:random-line.svg" /> -->
       <img src="https://api.iconify.design/fe:random.svg" />
     </div>
-    <div class="status-icon" @click="">
+    <div class="status-icon" @click="autoPlay">
       <img src="https://api.iconify.design/line-md:play.svg" />
     </div>
 
-    <div class="status-icon" @click="">
+    <div class="status-icon" @click="audioPlay">
       <img src="https://api.iconify.design/line-md:volume-high.svg" />
+      <audio
+        ref="audioRef"
+        :autoplay="autoplay"
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+      />
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const audioRef = ref()
+  const autoplay = ref(false)
+
+  function audioPlay() {
+    console.log(`🚀 ~ audioPlay ~ audioPlay:`)
+
+    audioRef.value.play()
+  }
+
+  function autoPlay() {
+    autoplay.value = !autoplay.value
+    console.log(`🚀 ~ autoPlay ~ autoplay.value:`, autoplay.value)
+  }
+</script>
 
 <style lang="scss" scoped></style>
