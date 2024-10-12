a020a05845d4b3954b22e67af5887f4fbd254772
Juche <azhucheng1@qq.com>
2024-10-12 14:44:05 +0800

    调整图片指示样式 & 添加自动播放倒计时视图

--- src/components/Panels/Play.vue
+++ src/components/Panels/Play.vue
@@ -22,7 +22,7 @@
     :effect="'cube'"
     :cubeEffect="{
       shadow: true,
-      slideShadows: true,
+      slideShadows: false,
       shadowOffset: 20,
       shadowScale: 0.94,
     }"
@@ -33,12 +33,14 @@
     :modules="modules"
     :navigation="!autoplay"
     :pagination="{
-      clickable: !autoplay,
-      type: 'fraction', // 数字分页
+      // clickable: !autoplay,
+      // type: 'fraction', // 数字分页
+      dynamicBullets: true,
     }"
     :simulateTouch="!autoplay"
     :slidesPerView="1"
     :zoom="true"
+    @autoplayTimeLeft="onAutoplayTimeLeft"
     @swiper="onSwiper"
     @slideChangeTransitionEnd="onSlideChangeTransitionEnd"
   >
@@ -57,6 +59,15 @@
         <img :src="item.src" />
       </div>
     </swiper-slide>
+
+    <template #container-end>
+      <div class="autoplay-progress">
+        <svg viewBox="0 0 48 48" ref="progressCircle">
+          <circle cx="24" cy="24" r="20"></circle>
+        </svg>
+        <span ref="progressContent"></span>
+      </div>
+    </template>
   </swiper>
 </template>
 <script setup lang="ts">
@@ -84,6 +95,7 @@
     Pagination,
     Zoom,
   } from 'swiper/modules'
+  import { ref } from 'vue'
 
   const modules = [
     Autoplay,
@@ -95,6 +107,13 @@
     Navigation,
     Zoom,
   ]
+
+  const progressCircle = ref(null)
+  const progressContent = ref(null)
+  const onAutoplayTimeLeft = (s: any, time: number, progress: number) => {
+    ;(progressCircle.value as any).style.setProperty('--progress', 1 - progress)
+    ;(progressContent.value as any).textContent = `${Math.ceil(time / 1000)}s`
+  }
 
   function onSwiper() {
     console.log('swiper initialized')
@@ -154,5 +173,36 @@
     font-size: 22px;
     font-weight: bold;
     color: #fff;
+  }
+
+  .autoplay-progress {
+    background: #ff03;
+    position: absolute;
+    right: 16px;
+    bottom: 16px;
+    z-index: 10;
+    width: 48px;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    color: var(--swiper-theme-color);
+  }
+
+  .autoplay-progress svg {
+    --progress: 0;
+    position: absolute;
+    left: 0;
+    top: 0px;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    stroke-width: 4px;
+    stroke: var(--swiper-theme-color);
+    fill: none;
+    stroke-dashoffset: calc(125.6px * (1 - var(--progress)));
+    stroke-dasharray: 125.6;
+    transform: rotate(-90deg);
   }
 </style>
